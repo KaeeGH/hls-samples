cb2ae73105715bc4330f6e336a8253b488df6ff9
KaeeGH <hoshi03918@gmail.com>
2020-11-15 14:09:15 +0900

    tサーバーアプリケーションにtest0のm3u8を返す機能を実装

--- hls-server/src/index.ts
+++ hls-server/src/index.ts
@@ -1,12 +1,14 @@
 import * as http from 'http'
+import * as fs from 'fs'
 
 const port = 3000
 const server = http.createServer()
 
 server.on('request', (request, response) => {
   response.statuCode = 200
-  response.setHeader('Content-Type', 'text/plain')
-  response.write('Hello, world!')
+  response.setHeader('Content-Type', 'application/vnd.apple.mpegurl')
+  const m3u8 = fs.readFileSync('./m3u8/m3u8/test0/test0.m3u8')
+  response.write(m3u8)
   response.end()
 })
 
